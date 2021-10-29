a7f29c206759b1e932b1eb8a2d0ecd0a7d8e63e1
ZelinGitHub <2414217563@qq.com>
2021-10-29 18:55:51 +0800

    abc

--- JJCPP/array_test.cpp
+++ JJCPP/array_test.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 
-void TestCArray() {
+int a[10];
+constexpr unsigned sz = 42;
+int* parr[sz];
+
+
+
+
+void TestArrayPlus() {
 	int arr[5] = { 1,2,3,4,5 };
 	int* p = arr + 2;
 	std::cout << *p << std::endl;
